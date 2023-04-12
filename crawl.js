a5d97950987f49d345e4ecf4d74cc7a21eba9d08
Konstantin Shambazov <k.shambazov@gmail.com>
2023-04-12 11:19:10 +0900

    feat: add crawl single page functionality with error handling

--- crawl.js
+++ crawl.js
@@ -1,6 +1,33 @@
 const url = require('url');
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(pageUrl) {
+  console.log(`I'm crawling over ${pageUrl}`);
+
+  try {
+    const resp = await fetch(pageUrl);
+
+    if (resp.status >= 400) {
+      console.log(
+        `Error when trying to fetch ${pageUrl} with status ${resp.status}`
+      );
+      return;
+    }
+
+    const contentType = resp.headers.get('content-type');
+    if (!contentType.includes('text/html')) {
+      console.log(
+        `Wrong content-type: ${contentType}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(`Error when trying to fetch ${pageUrl}: ${err.message}`);
+  }
+}
+
 function normalizeUrl(urlString) {
   let parsedUrl = url.parse(urlString);
 
@@ -40,10 +67,11 @@ function getUrlsFromHtml(htmlBody, baseUrl) {
       }
     }
   }
-  return urls
+  return urls;
 }
 
 module.exports = {
   normalizeUrl,
   getUrlsFromHtml,
+  crawlPage,
 };
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+const {crawlPage} = require ('./crawl.js');
+
 function main() {
   if (process.argv.length !== 3) {
     console.log('Please provide a valid URL');
     process.exit(1);
   }
-
   const baseUrl = process.argv[2]
-  console.log(`I'm crawling all over ${baseUrl}`);
+  crawlPage(baseUrl)
 }
 main();
