f4a80d7b6287489d8557d96302d3d63affa37a66
Konstantin Shambazov <k.shambazov@gmail.com>
2023-04-12 18:45:37 +0900

    feat: add printReport functionality

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const { crawlPage } = require('./crawl.js');
+const { printReport } = require('./report.js');
+
 
 async function main() {
   if (process.argv.length !== 3) {
@@ -7,8 +9,8 @@ async function main() {
   }
   const baseUrl = process.argv[2];
   const pages = await crawlPage(baseUrl, baseUrl, {});
-  for (const page of Object.entries(pages)){
-    console.log(page);
-  }
+
+  printReport(pages)
+
 }
 main();
--- report.js
+++ report.js
@@ -1,24 +1,40 @@
 function sortPages(obj) {
-    const entries = Object.entries(obj);
+  const entries = Object.entries(obj);
 
-    const sortByValueAndKey = (a, b) => {
-        if (a[1] === b[1]) {
-            for (let i = 0; i < Math.min(a[0].length, b[0].length); i++) {
-                if (a[0].charAt(i) < b[0].charAt(i)) {
-                    return -1;
-                } else if (a[0].charAt(i) > b[0].charAt(i)) {
-                    return 1;
-                }
-            }
-            return a[0].length - b[0].length;
-        } else {
-            return b[1] - a[1];
+  const sortByValueAndKey = (a, b) => {
+    if (a[1] === b[1]) {
+      for (let i = 0; i < Math.min(a[0].length, b[0].length); i++) {
+        if (a[0].charAt(i) < b[0].charAt(i)) {
+          return -1;
+        } else if (a[0].charAt(i) > b[0].charAt(i)) {
+          return 1;
         }
-    };
+      }
+      return a[0].length - b[0].length;
+    } else {
+      return b[1] - a[1];
+    }
+  };
 
-    const sortedEntries = entries.sort(sortByValueAndKey);
+  const sortedEntries = entries.sort(sortByValueAndKey);
 
-    return sortedEntries;
+  return sortedEntries;
 }
 
-module.exports = {sortPages}
+function printReport(pages) {
+  console.log('============');
+  console.log('REPORT');
+  console.log('============');
+
+  const sortedPages = sortPages(pages);
+
+  for (const page of sortedPages) {
+    console.log(`Found ${page[1]} links to the page: ${page[0]}`);
+  }
+
+  console.log('============');
+  console.log('END REPORT');
+  console.log('============');
+}
+
+module.exports = { sortPages, printReport };
